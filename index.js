ca39686bf7ff7add8b92af88235dbc2d16b4b659
Andrew <andrew.i.funke@gmail.com>
2023-07-10 08:27:27 -0700

    Array Find Method lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+const record = [
+  { year: "2015", result: "W"},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"},
+  //...
+]
+
+function superbowlWin(objectArray){
+   const returnValue = objectArray.find(element => element.result === 'W');
+   if (returnValue){
+    return returnValue.year
+   }
+//    else return undefined;
+   
+}
+
+// superbowlWin(record);
+
